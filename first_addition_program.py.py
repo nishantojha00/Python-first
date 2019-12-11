9a085359d30a280e0bffe3d84319dc2c965a49d5
unknown <nishantojha00@gmail.com>
2019-12-11 12:12:21 +0530

    updating the new version of addition

---
+++ first_addition_program.py.py
@@ -0,0 +1,14 @@
+
+"""
+Purpose:- Second version of addition.
+Author :- Nishant Kumar Ojha
+Date   :- 11 December 2019
+"""
+
+a = int(input ("Put the value of a"))
+
+b = int(input ("Put the value of b"))
+
+c = a + b
+
+print("addition of a + b is", c)
